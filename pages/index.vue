6e2587cb5252bf13b9a5a733e2e3fb05eb7cca27
kensuke goto <kensuke.goto.1981@gmail.com>
2020-09-22 16:24:17 +0900

    変更：クラス名を整理・子コンポーネントの変更を保存

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,16 @@
     <TextArea 
       v-for="(item,key) of data"
       :key="key"
-      :content="item"
+      :content="item.content"
       @doUpdate="e => {doUpdate(e,key)}"
     />
+    <section id="tools" class="b-tools">
+      <ul class="m-tools">
+        <li class="item"><a class="btn btn--add-title">タイトル追加</a></li>
+        <li class="item"><a class="btn btn--add-text">テキスト追加</a></li>
+        <li class="item"><a class="btn btn--save" @click="doSave">保存</a></li>
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -14,44 +21,53 @@ export default {
 
   data: () => {
 
-    const data = [
+    const data = shapeData([
       {"block":0,"tag":"p","class":"","content":"中東における、金融や物流の中心地ドバイ。"},
       {"block":0,"tag":"p","class":"bgBlue","content":"上東における、金融や物流の中心地ドバイ。"},
-    ];
-
-    let items = data.reduce((list,item,index)=> {
-      if(item.tag === "p"){
-        list[item.block] = !list[item.block] ? [] : list[item.block];
-        list[item.block].push(item);
-      } else {
-        list[item.block] = item;
-      }
-      return list
-    },[]);
-
-    const items2 = items.reduce((list,item) => {
-      let value = "";
-      if(Array.isArray(item)){
-        if(item){
-          item.forEach(p => {
-            value += `<${p.tag} class="${p.class}">${p.content}</${p.tag}>`;
-          });
-        }
-      }
-
-      list.push(value);
-
-      return list;
-    },[])
+    ]);
 
     return {
-      data : items2
+      data
     }
+
   },
   methods:{
-    doUpdate(e,index){
-      console.log(e);
-    } 
+    doUpdate(value,index){
+      this.data[index].update = value
+    },
+    doSave(){
+      console.log(this.data);
+    }
   }
+}
+
+function shapeData(data){
+
+  let blocks = data.reduce((list,item,index)=> {
+    if(item.tag === "p"){
+      list[item.block] = !list[item.block] ? [] : list[item.block];
+      list[item.block].push(item);
+    } else {
+      list[item.block] = item;
+    }
+    return list;
+  },[]);
+
+  blocks = blocks.reduce((list,block) => {
+    let value = "";
+    if(Array.isArray(block)){
+      if(block){
+        block.forEach(item => {
+          value += `<${item.tag} class="${item.class}">${item.content}</${item.tag}>`;
+        });
+      }
+    }
+
+    list.push({ content: value, update: ""});
+    return list;
+  },[])
+
+  return blocks;
+
 }
 </script>
